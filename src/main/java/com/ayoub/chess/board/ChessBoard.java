ef2af33640f3be3ae31afd30a508bfa9df2fba91
ayoub ouabi <ayoub.ouabi06@gmail.com>
2023-09-21 11:10:27 +0100

    git start work on piece moves

--- src/main/java/com/ayoub/chess/board/ChessBoard.java
+++ src/main/java/com/ayoub/chess/board/ChessBoard.java
@@ -12,10 +12,6 @@ import java.util.Map;
 public class ChessBoard {
     int boardSize=8;
 
-    Map<PieceType,List<Move>> getMove(){
-        Map<PieceType,List<Move>> listMap=new HashMap<>();
-        List<Move> move=new ArrayList<>();
-        for
-    }
+
 }
 
--- src/main/java/com/ayoub/chess/pieces/ChessPiece.java
+++ src/main/java/com/ayoub/chess/pieces/ChessPiece.java
@@ -3,6 +3,11 @@ package com.ayoub.chess.pieces;
 
 import com.ayoub.chess.enums.Color;
 import com.ayoub.chess.enums.PieceType;
+import com.ayoub.chess.moves.Move;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ChessPiece {
     private PieceType pieceType;
@@ -24,5 +29,9 @@ public class ChessPiece {
     @Override
     public String toString() {
          return color.toString() + " " + pieceType.toString();
+    }
+
+    public Map<PieceType,List<Move>> getMoves(){
+        Map<PieceType,List<Move>> pieceMove=new HashMap<>();
     }
 }
